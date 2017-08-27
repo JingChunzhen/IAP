371c7d6549c317e5cb9f82e365ab815e16ccace4
JingChunzhen <1051949460@qq.com>
2017-08-28 00:16:33 +0800

    新增加了数据部分，和生成的数据

---
+++ data/readExample.py
@@ -0,0 +1,16 @@
+﻿#coding: utf-8
+
+file=open(r'C:\\Users\\lenovo\\workspace1\\Qbs\\src\\共享单车\\共享单车keywords.txt','r',encoding='utf-8')
+data=file.read()
+keywords=data.split(' ')
+print(len(keywords),keywords[1])
+
+file1=open(r'C:\\Users\\lenovo\\workspace1\\Qbs\\src\\共享单车\\共享单车keySentence.txt','r',encoding='utf-8')
+data1=file1.read()
+keySentence=data1.split('\n')
+print(len(keySentence),keySentence[1])
+'''
+Created on 2017年7月11日
+
+@author: lenovo
+'''
